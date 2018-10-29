32da3d9cb046b5fedaa56d4b4208a3951c202e85
salevizo <alev.sophia@gmail.com>
2018-10-30 01:26:50 +0200

    find lat lon form 3035 proj to 4326

--- natural/natura.py
+++ natural/natura.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import geopandas
 from numpy import asarray
-
+from pyproj import Proj, transform
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
@@ -16,7 +16,8 @@ from shapely.geometry.polygon import Polygon
 
 db=('doi105281zenodo1167595')
 query="SELECT  st_x(st_centroid(geom)) as st_x ,st_y(st_centroid(geom)) as st_y, geom  FROM natura2000.spatialfeatures LIMIT 10;"
-#3035
+#geometry(MultiPolygon,3035) 
+#EPSG:3035 to EPSG:4326 (lat/long) python
 query2="SELECT lat, lon FROM nari_dynamic WHERE mmsi=228231800 LIMIT 1;"
 
 
@@ -28,13 +29,29 @@ with con:
    
 print df_natura;
 
+df_natura['geom']=df_natura['geom'].to_crs(epsg=4326)
+
+
+
+print type(df_natura['geom'])
 point = Point(df_natura['st_x'][0], df_natura['st_y'][0])
 #point2 = Point(df_mmsi['lon'], df_mmsi['lat'])
 print(df_natura['geom'].contains(point))
-#print(df_natura['geom'].contains(point2))
-#vriskeis ta lon, lat, ftiaxneis point kai tsekareis an einai sto polygon
 
-df_natura.plot();
+
+
+#m.plot(x,y, marker ='o', markersize=6, markerfacecolor='red', linewidth=0)
+
+
+df_natura['geom'].plot();
 plt.show()
+
+###################
+
+
+
+
+
+
 
 
